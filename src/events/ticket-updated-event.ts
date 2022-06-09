83d4a61540874384576fac9a20c3892f5f4d5d29
djay <djay@boegismedia.com>
2022-06-09 22:26:12 +0700

    add event related code

---
+++ src/events/ticket-updated-event.ts
@@ -0,0 +1,10 @@
+import { Subjects } from "./subjets";
+export interface TicketUpdateEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
